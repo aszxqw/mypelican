abb7378b3c48008263df9fcdb64cefe049d3daea
wyy <wuyanyi09@gmail.com>
2013-08-23 10:50:15 -0700

    init

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'aszxqw'
+SITENAME = u'YesWeCode'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+         )
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/aszxqw'),
+          ('Weibo', 'http://weibo.com/buptwyy'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
